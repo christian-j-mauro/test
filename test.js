f4299914f1e4aff141d6852b9f1c0a9a8cfa317d
Christian <christian.j.mauro@gmail.com>
2022-01-03 00:43:50 -0500

    Name question

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
+var rs = require('readline-sync');
+
 var num = 24;
 var newNum = 32;
 
 console.log(num + newNum);
+
+var name = rs.question("what's your name? ");
+
+console.log("Hello " + name + "!");
